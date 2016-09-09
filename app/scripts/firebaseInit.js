648c4fdbc452302edf44d028023ecadcc45cbd8e
kennedyb <brian.kennedy@wwt.com>
2016-09-09 00:34:00 -0500

    fixing lint and firebaseInit inside of IIFE

--- app/scripts/firebaseInit.js
+++ app/scripts/firebaseInit.js
@@ -1,8 +1,12 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyAoRAWmkrHNtwNU7tlTpHSSyLNnEM-ra7c",
-  authDomain: "swc-shockball.firebaseapp.com",
-  databaseURL: "https://swc-shockball.firebaseio.com",
-  storageBucket: "swc-shockball.appspot.com",
-};
-firebase.initializeApp(config);
+'use strict';
+
+(function(firebase){
+    // Initialize Firebase
+    var config = {
+      apiKey: 'AIzaSyAoRAWmkrHNtwNU7tlTpHSSyLNnEM-ra7c',
+      authDomain: 'swc-shockball.firebaseapp.com',
+      databaseURL: 'https://swc-shockball.firebaseio.com',
+      storageBucket: 'swc-shockball.appspot.com',
+    };
+    firebase.initializeApp(config);
+}(firebase));
