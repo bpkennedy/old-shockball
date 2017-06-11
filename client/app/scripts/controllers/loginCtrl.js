5384cb2be5bd5cce1099628d1bc3d658dafe6485
kennedyb <brian.kennedy@wwt.com>
2017-06-11 15:52:39 -0500

    auth added via angularfire  service

--- client/app/scripts/controllers/loginCtrl.js
+++ client/app/scripts/controllers/loginCtrl.js
@@ -8,5 +8,98 @@
 * Controller of the shockballApp
 */
 angular.module('shockballApp')
-.controller('LoginCtrl', function () {
+.controller('LoginCtrl', function ($scope, auth, currentUser) {
+    var vm = this;
+    vm.message = null;
+    vm.error = null;
+    vm.email = null;
+    vm.password = null;
+    vm.handle = null;
+    vm.createUser = createUser;
+    vm.deleteUser = deleteUser;
+    vm.signIn = signIn;
+    vm.signOut = signOut;
+    vm.updateEmail = updateEmail;
+    vm.updatePassword = updatePassword;
+    vm.getUserToken = getUserToken;
+
+    function signIn() {
+        clearValidation();
+
+        // Sign in with email/password
+        auth.$signInWithEmailAndPassword(vm.email, vm.password)
+        .then(function(firebaseUser) {
+            vm.message = "User signed in as: " + firebaseUser.email;
+        }).catch(function(error) {
+            vm.error = "Error: " + error;
+        });
+    }
+
+    function signOut() {
+        clearValidation();
+
+        // Sign out.  Null is returned
+        auth.$signOut();
+        $scope.$applyAsync();
+        if (!currentUser) {
+            vm.message = "User signed out.";
+        } else {
+            vm.error = "Something went wrong, failed to sign out.";
+        }
+    }
+
+    function updateEmail() {
+        clearValidation();
+        auth.$updateEmail(vm.email).then(function() {
+            vm.message = "Email changed successfully!";
+        }).catch(function(error) {
+            vm.error = "Error: " + error;
+        });
+    }
+
+    function updatePassword() {
+        clearValidation();
+        auth.$updatePassword(vm.password).then(function() {
+            vm.message = "Password changed successfully!";
+        }).catch(function(error) {
+            vm.error = "Error: " + error;
+        });
+    }
+
+    function createUser() {
+        clearValidation();
+
+        // Create a new user
+        auth.$createUserWithEmailAndPassword(vm.email, vm.password)
+        .then(function(firebaseUser) {
+            vm.message = "User created with uid: " + firebaseUser.uid;
+        }).catch(function(error) {
+            vm.error = "Error: " + error;
+        });
+    }
+
+    function deleteUser() {
+        clearValidation();
+
+        // Delete the currently signed-in user
+        auth.$deleteUser().then(function() {
+            vm.message = "User deleted";
+        }).catch(function(error) {
+            vm.error = "Error: " + error;
+        });
+    }
+
+    function getUserToken() {
+        auth.$getAuth().getToken(true).then(function(idToken) {
+            vm.message = "User idToken is: " + idToken;
+        }).catch(function(error) {
+            vm.error = "Error: " + error;
+        });
+    }
+
+    function clearValidation() {
+        vm.message = null;
+        vm.error = null;
+    }
+
 });
