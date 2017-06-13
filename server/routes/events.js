50b48f48da9858aa334d46787175f0975f6bea52
kennedyb <brian.kennedy@wwt.com>
2017-06-13 14:31:10 -0500

    events using POST and client-server token validation and Joi library for schema checks on the data.

--- server/routes/events.js
+++ server/routes/events.js
@@ -2,10 +2,23 @@ var express = require('express');
 var admin = require("firebase-admin");
 var router = express.Router();
 var querybase = require('querybase');
+const Joi = require('joi');
+
 
 // Get a database reference to our posts
 var db = admin.database();
 var ref = db.ref("events");
+
+const eventSchema = Joi.object().keys({
+    actor: Joi.string().alphanum().required(),
+    oppActor: Joi.string().alphanum().allow(null),
+    secondaryOppActor: Joi.string().alphanum().allow(null),
+    type: Joi.string().alphanum().required(),
+    match: Joi.string().alphanum().allow(null),
+    team: Joi.string().alphanum().allow(null),
+    oppTeam: Joi.string().alphanum().allow(null),
+    time: Joi.date().required()
+}).with('actor', 'type');
 
 
 /* GET events. */
@@ -33,10 +46,10 @@ router.get('/match/:uid', function(req, res, next) {
     }
 });
 
-// GET primarySource events by playerId
-router.get('/primarySource/:uid', function (req, res, next) {
+// GET primarySource events by actor
+router.get('/actor/:uid', function (req, res, next) {
     if (req.params.uid) {
-        ref.orderByChild("primarySource").equalTo(req.params.uid).once("value", function(snapshot) {
+        ref.orderByChild("actor").equalTo(req.params.uid).once("value", function(snapshot) {
             res.send(snapshot.val());
         }, function (errorObject) {
           res.send(errorObject);
@@ -47,10 +60,10 @@ router.get('/primarySource/:uid', function (req, res, next) {
     }
 });
 
-// GET primarySource events by playerId
-router.get('/secondarySource/:uid', function (req, res, next) {
+// GET primarySource events by oppActor
+router.get('/oppActor/:uid', function (req, res, next) {
     if (req.params.uid) {
-        ref.orderByChild("secondarySource").equalTo(req.params.uid).once("value", function(snapshot) {
+        ref.orderByChild("oppActor").equalTo(req.params.uid).once("value", function(snapshot) {
             res.send(snapshot.val());
         }, function (errorObject) {
           res.send(errorObject);
@@ -59,6 +72,40 @@ router.get('/secondarySource/:uid', function (req, res, next) {
     } else {
         res.send({ message: 'Failed to pass in a league uid' });
     }
+});
+
+// GET primarySource events by secondaryOppActor
+router.get('/secondaryOppActor/:uid', function (req, res, next) {
+    if (req.params.uid) {
+        ref.orderByChild("secondaryOppActor").equalTo(req.params.uid).once("value", function(snapshot) {
+            res.send(snapshot.val());
+        }, function (errorObject) {
+          res.send(errorObject);
+          console.log("The read failed: " + errorObject.code);
+        });
+    } else {
+        res.send({ message: 'Failed to pass in a league uid' });
+    }
+});
+
+//POST new event
+router.post('/test', function(req, res) {
+    admin.auth().verifyIdToken(req.query.idToken).then(function(decodedToken) {
+        const result = Joi.validate(req.body, eventSchema);
+        if (result.error === null) {
+            ref.push().set(req.body, function (errorObject) {
+                if (errorObject) {
+                    res.send(errorObject);
+                } else {
+                    res.send({ message: 'success!' });
+                }
+            });
+        } else {
+            res.send({ message: 'invalid data format' });
+        }
+    }).catch(function(error) {
+        res.send({ message: 'invalid idToken' });
+    });
 });
 
 module.exports = router;
